1f2f6d27545ce36d6b38275905f0046af1f3a612
KaleyXZ <zheng.kaley.x@gmail.com>
2017-07-14 19:05:39 +1000

    save

---
+++ PHP/Tutorial/src/01_Hellp.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Online PHP Script Execution</title>
+    </head>
+    <body>
+        <?php
+            echo "<h1>Hello, PHP!</h1>";
+        ?>
+    </body>
+</html>
